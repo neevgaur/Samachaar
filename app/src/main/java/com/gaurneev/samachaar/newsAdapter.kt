a3e82c15c542bbbefd714a7c54677d5384427e91
Neev Gaur <gaurneev@gmail.com>
2024-08-30 22:08:03 +0530

    updation github actions

--- app/src/main/java/com/gaurneev/samachaar/newsAdapter.kt
+++ app/src/main/java/com/gaurneev/samachaar/newsAdapter.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
